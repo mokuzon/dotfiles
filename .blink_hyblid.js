82f1054d748d309bb960b1b3595891578b7833cc
Motoi Okuzono <motoi.okuzono+github@gmail.com>
2021-02-26 01:42:45 +0900

    Update

---
+++ .blink_hyblid.js
@@ -0,0 +1,22 @@
+black    = '#373b40'
+red      = '#cc6565'
+green    = '#b5bc67'
+yellow   = '#f0c673'
+blue     = '#81a2bd'
+magenta  = '#b294ba'
+cyan     = '#9fc9c3'
+white    = '#c5c8c6'
+lightBlack   = '#373b40'
+lightRed     = '#cc6565'
+lightGreen   = '#b5bc67'
+lightYellow  = '#f0c673'
+lightBlue    = '#81a2bd'
+lightMagenta = '#b294ba'
+lightCyan    = '#9fc9c3'
+lightWhite   = '#c5c8c6'
+
+t.prefs_.set('color-palette-overrides', [black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite]);
+
+t.prefs_.set('foreground-color', "#b7bcba");
+t.prefs_.set('background-color', "#161719");
+t.prefs_.set('cursor-color', 'rgba(183,188,186,0.5)');
